c223303d287d2cf6498a68692eddc0ea38d6b12d
doliveira <doliveira@cdts.fiocruz.br>
2017-03-29 16:15:00 -0300

    Componentes Controlados e Não controládos

--- ex_07_component_state/index.jsx
+++ ex_07_component_state/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Field from './field'
+import ClassComponent from './classComponent'
 
 
 
 ReactDom.render(
-     <Field  initialValue='Teste' />
+     <ClassComponent lavel='Contador' initialValue={10} />
     , document.getElementById('app'))
